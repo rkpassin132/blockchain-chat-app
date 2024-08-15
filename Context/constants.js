65f6c22879d01f67a6a8365af5620ebd14918290
Rahul kumar <rkpassin132@gmail.com>
2024-08-15 23:06:33 +0530

    deployment changes

--- Context/constants.js
+++ Context/constants.js
@@ -1,4 +1,6 @@
-import chatAppJSON from "../artifacts/contracts/ChatApp.sol/ChatApp.json";
+// import chatAppJSON from "../artifacts/contracts/ChatApp.sol/ChatApp.json";
+import chatAppJSON from "../Context/ChatAppAbi.json";
 
-export const ChatAppAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-export const ChatAppABI = chatAppJSON.abi;
+export const ChatAppAddress = "0xdf2cBf666d4cffBC2F373C5068C72E607aFF9531"; // This is testnet deploy contract address
+// export const ChatAppAddress = "0xdf2cBf666d4cffBC2F373C5068C72E607aFF9531"; // This is testnet deploy contract address
+export const ChatAppABI = chatAppJSON;
--- README.md
+++ README.md
@@ -18,3 +18,10 @@ npx hardhat run scripts/deploy.js
 - run local hardhat network and deploy contract
 - connect hardhat account by improting using private key
 - create account in Message app
+
+### some importtant link
+
+You can deploy your contrat from remix for testnet. Just change you network contect to an account. Then compile contract and deploy contract on that network.
+
+- [Testnet bscscan](https://testnet.bscscan.com/) - use this network to deploy you contract on testnet
+- [bnb faucet](https://www.bnbchain.org/en/testnet-faucet) - use this link to get bnb network faucet
