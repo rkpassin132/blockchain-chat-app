0c3e6662a8781ba6480738472529ad91e339a3cc
Rahul kumar <rkpassin132@gmail.com>
2024-08-17 14:27:58 +0530

    git commit -m contant changes

--- Context/constants.js
+++ Context/constants.js
@@ -1,7 +1,6 @@
-import chatAppJSON from "../artifacts/contracts/ChatApp.sol/ChatApp.json";
+// import chatAppJSON from "../artifacts/contracts/ChatApp.sol/ChatApp.json";
 import chatAppJSONProd from "../Context/ChatAppAbi.json"; // Production Testnet deployed ABI
 
 export const enviroment = process.env.NEXT_PUBLIC_ENVIRONMENT;
 export const ChatAppAddress = process.env.NEXT_PUBLIC_CHAT_APP_ADDRESS;
-export const ChatAppABI =
-  enviroment == "local" ? chatAppJSON.abi : chatAppJSONProd;
+export const ChatAppABI = enviroment == chatAppJSONProd;
